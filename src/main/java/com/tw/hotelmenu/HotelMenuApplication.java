096d6d67f02fe0f00039172e7df015e80c8e28f5
vsravani <sravani.krishnan@gmail.com>
2022-06-24 21:52:58 +0530

    Add. Initial Commit for hotel menu project

---
+++ src/main/java/com/tw/hotelmenu/HotelMenuApplication.java
@@ -0,0 +1,13 @@
+package com.tw.hotelmenu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelMenuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelMenuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tw/hotelmenu/HotelMenuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tw.hotelmenu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelMenuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
